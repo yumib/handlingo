5d3f37bd4e1d46fbd85c03270a6078400de2623b
yumib <ybalotro@gmail.com>
2025-03-30 22:39:37 -0500

    instances of data displayed for each type of section page

--- handlingo/app/api/section/[sectionId]/[questionNum]/route.ts
+++ handlingo/app/api/section/[sectionId]/[questionNum]/route.ts
@@ -2,16 +2,26 @@
 import { NextResponse } from "next/server";
 import { getQuestionByNum } from "@/utils/databaseQuery";
 
-export async function GET(request: Request, { params }: { params: { sectionId: string } }) {
-    const url = new URL(request.url);
-    const questionId = url.searchParams.get('q'); // Get query parameter `q`
+export async function GET(request: Request, context: { params: { sectionId?: string; questionNum?: string } }) {
+    // get parameters from url
+    let { params } = context;
+    params = await params
+    if (!params) {
+        return NextResponse.json({ error: "Missing sectionId parameter" }, { status: 400 });
+    }
 
-    if (!questionId) {
-        return NextResponse.json({ error: "Missing question id" }, { status: 400 });
+    // convert section id and question number into int for queries
+    const sectionId = Number(params.sectionId);
+    const questionNumber = Number(params.questionNum);
+
+    if (!questionNumber || !sectionId) {
+        return NextResponse.json({ error: "Missing question num or section id" }, { status: 400 });
     }
 
     try {
-        const question = await getQuestionByNum(Number(questionId), Number(params.sectionId));
+
+        // get question data  w/ questionNumber and sectionId from db queries on './utils/databaseQuery.ts'
+        const question = await getQuestionByNum(questionNumber, sectionId);
 
         if (!question) {
             return NextResponse.json({ error: "Question not found" }, { status: 404 });
