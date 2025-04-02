909bde023375a46a833333cf21dbcf9922fd34b2
Cassandra Garza <cassandra.ygarza@gmail.com>
2025-04-01 22:55:32 -0500

    sign-in page completed

--- handlingo/app/auth-pages/sign-in/page.tsx
+++ handlingo/app/auth-pages/sign-in/page.tsx
@@ -15,48 +15,75 @@ export default async function SignIn(props: { searchParams: Promise<Message> }) 
   const searchParams = await props.searchParams;
 
   return (
-    // background image
     <div className="flex w-screen items-center justify-center min-h-screen relative">
-      {/* Background Image */}
-      <Image
+    {/* Background Image */}
+    <Image
         src="/assets/login-background.png" // Use the imported image
         alt="Login Background"
-        className="absolute top-0 left-0 w-full h-full object-full"
+        className="absolute top-0 left-0 w-full h-full object-full z-0"
         width= {800}
         height={800}
       />
-    <form className="flex-1 flex flex-col min-w-48 max-w-72">
-      <h1 className="text-2xl font-medium">Log in</h1>
-      <p className="text-sm text-foreground">
-        Don't have an account?{" "}
-        <Link className="text-foreground font-medium underline" href="/auth-pages/sign-up">
-          Sign Up
-        </Link>
-      </p>
-      <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-        <Label htmlFor="username">Username</Label>
-        <Input name="username" placeholder="Your username" required />
-        <div className="flex justify-between items-center">
-          <Label htmlFor="password">Password</Label>
-          <Link
-            className="text-xs text-foreground underline"
-            href="/forgot-password"
-          >
-            Forgot Password?
+
+    <form className="relative flex-1 flex flex-col min-w-48 max-w-96 z-10">
+      {/* Handlingo Title */}
+      <div className="flex justify-center">
+          <Image
+            src="/assets/handlingo-title.png"
+            alt="Handlingo Login Title"
+            className="mb-10 w-96 h-auto"
+            width={600} // Adjust the width as needed
+            height={150}  // Adjust the height as needed
+          />
+        </div>
+
+      {/* Log In Box */}
+      <div className="flex flex-col items-center space-y-4 p-6 bg-lightBlue rounded-lg">
+        
+        {/* Log In / Sign Up button */}
+        <div className="flex items-center justify-between w-40">
+          <h1 className="w-1/2 text-sm font-semibold font-fira text-black pl-5 mb-4 pb-2 border-b-2 border-black">
+            Log in
+          </h1>
+
+          <Link className="w-1/2 text-sm font-semibold font-fira text-gray mb-4 pb-2 border-b-2 border-lightBlue max-w-max" href="/auth-pages/sign-up">
+            Sign Up
           </Link>
         </div>
-        <Input
-          type="password"
-          name="password"
-          placeholder="Your password"
-          required
-        />
+
+        {/* User Form Input Section */}
+        <div className="flex flex-col w-full pl-5 pr-5 gap-2 [&>input]:mb-5 mt-8">
+          {/* Email */}
+          <Label className="font-nunito font-medium" htmlFor="email">Email</Label>
+          <Input className="border-b-1 border-black" 
+            name="email" 
+            placeholder="Your email" 
+            required 
+            /> {/* changed from 'username' to 'email' !!!!! */}
+
+          {/* Password */}
+          <Label className="font-nunito font-medium" htmlFor="password">Password</Label>
+          <Input className="border-b-1 border-black"
+            type="password"
+            name="password"
+            placeholder="Your password"
+            required
+          /> 
+        </div>
+
+        {/* Submit Button */}
         {/* goes to file ./app/actions.ts to handle signing in user, function signInAction */}
-        <SubmitButton pendingText="Logging In..." formAction={signInAction}>
+        <SubmitButton className="w-[calc(95%-20px)] h-10" pendingText="Logging In..." formAction={signInAction}>
           Log in
         </SubmitButton>
-        <FormMessage message={searchParams} />
-      </div>
+
+        {/* Forgot Password*/}
+        <Link className="text-xs text-foreground underline" href="/forgot-password">
+              Forgot your password?
+        </Link>
+
+      <FormMessage message={searchParams} />
+      </div> {/* end blue box container */}
     </form> 
   </div>           
   );
--- handlingo/tailwind.config.ts
+++ handlingo/tailwind.config.ts
@@ -18,6 +18,10 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        fira: ["Fira Sans", "sans-serif"],
+        nunito: ["Nunito", "sans-serif"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
